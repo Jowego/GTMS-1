232adac3eae33abf3140137019195d6104f4b788
Jörg Weisner <jorgweisner@MacBookPro.fritz.box>
2025-07-02 13:42:49 +0200

    Clean slate with GTMS project

--- score_logic.py
+++
@@ -1,9 +0,0 @@
-def calculate_scores(answers):
-    ga_score = 0
-    rs_score = 0
-    for key, value in answers.items():
-        if key.startswith('ga_'):
-            ga_score += int(value)
-        elif key.startswith('rs_'):
-            rs_score += int(value)
-    return ga_score, rs_score
